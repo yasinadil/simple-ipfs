b7a2c5aabdf0c04d84702c709d863236beb76e8b
yasinadil <adilyasin205@gmail.com>
2022-07-21 16:41:45 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {create as ipfsHttpClient} from 'ipfs-http-client';
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+              <label>
+                <p>Upload File</p>
+                <input placeholder='input' type="file"
+                onChange={async(e) => {
+                  const file = e.target.files[0]
+                  console.log(file)
+                  console.log(client)
+                  const add = await client.add(file);
+                  const url = `https://ipfs.infura.io/ipfs/${add.path}`;
+                  console.log(url)
+                }}>
+
+                </input>
+
+              
+              </label>
     </div>
   );
 }
